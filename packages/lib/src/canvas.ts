07189888e438f694bbd5eb1e2c7f014ea27cb674
Grant <grant@sc07.company>
2024-07-12 02:43:09 -0600

    hot patch: magic number cooldown

--- packages/lib/src/canvas.ts
+++ packages/lib/src/canvas.ts
@@ -9,13 +9,17 @@ export const CanvasLib = new (class {
    * @returns Seconds to take to give the pixel
    */
   getPixelCooldown(pixelNumber: number, config: ClientConfig) {
-    return pixelNumber * config.canvas.pixel.cooldown;
-    // const factorial = (n: number) => (n == 0 ? 1 : n * factorial(n - 1));
+    // return pixelNumber * config.canvas.pixel.cooldown;
+    // const factorial = (n: number): number => (n == 0 ? 1 : n * factorial(n - 1));
 
     // return (
     //   config.canvas.pixel.cooldown *
     //   config.canvas.pixel.multiplier *
     //   (2 + pixelNumber + factorial(pixelNumber))
     // );
+
+    // oh god last minute change to match activity cooldown
+    // 100 = user count
+    return (2.5 * Math.sqrt(100 + 11.96) + 6.5) * 1 * pixelNumber;
   }
 })();
