76aac2cd3869e0cd70e268b47435d3d64787c7df
Ategon <benjamin@barbeau.net>
2024-07-15 11:48:55 -0400

    Add dark mode

---
+++ packages/client/src/components/Header/HeaderLeft.tsx
@@ -0,0 +1,32 @@
+import { Button } from "@nextui-org/react";
+import { useAppContext } from "../../contexts/AppContext";
+import { AccountStanding } from "./AccountStanding";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Debug } from "@sc07-canvas/lib/src/debug";
+import { faInfoCircle, faTools } from "@fortawesome/free-solid-svg-icons";
+
+export const HeaderLeft = () => {
+  const { setInfoSidebar } = useAppContext();
+
+  return (
+    <div className="box gap-2 flex">
+      <AccountStanding />
+      <Button 
+        onPress={() => setInfoSidebar(true)}
+        variant="ghost"
+      >
+        <FontAwesomeIcon icon={faInfoCircle} />
+        <p>Info</p>
+      </Button>
+      {import.meta.env.DEV && (
+        <Button 
+          onPress={() => Debug.openDebugTools()}
+          variant="ghost"
+        >
+          <FontAwesomeIcon icon={faTools} />
+          <p>Debug Tools</p>
+        </Button>
+      )}
+    </div>
+  );
+};
