2ca6ef53393556a873aa1f57f302ce8a86d26d09
Grant <grant@sc07.company>
2024-07-15 16:26:32 -0600

    add stylesheet & fix toggle

--- packages/client/src/components/Overlay/PixelPulses.tsx
+++ packages/client/src/components/Overlay/PixelPulses.tsx
@@ -9,47 +9,51 @@ export const PixelPulses = () => {
   const [pulses, setPulses] = useState<JSX.Element[]>([]);
 
   useEffect(() => {
+    function handlePixel({ x, y, color }: Pixel) {
+      if (!pixelPulses) {
+        return;
+      }
+
+      const paletteColor = Canvas.instance?.Pallete.getColor(color);
+
+      const pulseStyle: CSSProperties = {
+        position: "absolute",
+        zIndex: "100",
+        left: x + "px",
+        top: y + "px",
+        width: "50px",
+        height: "50px",
+        border: `1px solid #${paletteColor?.hex || "000"}`, // default to black in the case it fails to load, but that shouldn't happen
+        borderRadius: "100px",
+        transform: "translate(-24.5px, -24.5px)",
+        animationName: "pixel-pulse",
+        animationTimingFunction: "ease-in-out",
+        animationDuration: "2s",
+        animationFillMode: "forwards",
+      };
+
+      // used in the case of two pixels coming through for the same position
+      // rare, but causes issues with react
+      // even if the pixels are close to eachother, the ms will be different
+      const timestamp = Date.now();
+
+      const pulseElement = (
+        <div key={`${x}-${y}-${timestamp}`} style={pulseStyle}></div>
+      );
+
+      setPulses((prevPulses) => [...prevPulses, pulseElement]);
+
+      setTimeout(() => {
+        setPulses((prevPulses) => prevPulses.slice(1)); // Remove the oldest pulse after 3700ms
+      }, 2500);
+    }
+
     network.on("pixel", handlePixel);
 
     return () => {
       network.off("pixel", handlePixel);
     };
-  }, []);
-
-  function handlePixel({ x, y, color }: Pixel) {
-    if (!pixelPulses) {
-      return;
-    }
-
-    const pulseStyle: CSSProperties = {
-      position: "absolute",
-      zIndex: "100",
-      left: x + "px",
-      top: y + "px",
-      width: "50px",
-      height: "50px",
-      border: `1px solid ${Canvas.instance?.Pallete.getColor(color)?.hex}`,
-      borderRadius: "100px",
-      transform: "translate(-24.5px, -24.5px)",
-      animationName: "pixel-pulse",
-      animationDuration: "4s",
-    };
-
-    // used in the case of two pixels coming through for the same position
-    // rare, but causes issues with react
-    // even if the pixels are close to eachother, the ms will be different
-    const timestamp = Date.now();
-
-    const pulseElement = (
-      <div key={`${x}-${y}-${timestamp}`} style={pulseStyle}></div>
-    );
-
-    setPulses((prevPulses) => [...prevPulses, pulseElement]);
-
-    setTimeout(() => {
-      setPulses((prevPulses) => prevPulses.slice(1)); // Remove the oldest pulse after 3700ms
-    }, 3700);
-  }
+  }, [pixelPulses]);
 
   return <div>{pulses}</div>;
 };
